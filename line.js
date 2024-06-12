b6a7c4b0e1f62ead77938129316d86f7498e803a
Geovanna <geovannasantosilv@gmail.com>
2024-06-12 05:32:09 -0300

    Minha mensagem

---
+++ line.js
@@ -0,0 +1,16 @@
+
+function setup() {
+
+createCanvas(200, 200);
+
+background("white");
+
+}
+
+function draw() {
+
+stroke("red");
+
+line(50, 100, 150, 100);
+
+}
---
+++ square.js
@@ -0,0 +1,16 @@
+
+function setup() {
+
+createCanvas(200, 200);
+
+background("white");
+
+}
+
+function draw() {
+
+stroke("blue");
+
+square(50, 50, 100);
+
+}
